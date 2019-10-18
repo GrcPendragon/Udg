36a8e96b7b122eaf3b738f4bd3fc0e1a8946da57
Fausto Garcia <fausto.garcia@alumnos.udg.mx>
2019-10-18 17:15:12 -0500

    Practica7_MatrizInversa, finalizada y entregada

--- Practica7_MatrizInversa/MainForm.vb
+++ Practica7_MatrizInversa/MainForm.vb
@@ -19,24 +19,34 @@ Public Partial Class MainForm
 		'
 	End Sub
 	
-	Sub cargar(ByRef mtr(,) As Double,ByRef mtrId(,) As Double, txt As TextBox)
-		Dim filas As Integer = txt.Lines.Length-1
-		Dim columnas As Integer = txt.Lines(0).Split(Convert.ToChar(",")).Length - 1
-		ReDim mtr(filas, columnas)
-		ReDim mtrId(filas, columnas)
-		
-		For	i = 0 To filas
-			For j = 0 To columnas
-				mtr(i,j) = Convert.ToDouble(txt.Lines(i).Split(Convert.ToChar(","))(j))
-				If i = j Then
-					mtrId(i,j) = 1
-				Else
-					mtrId(i,j) = 0
-				End If
+	Function cargar(ByRef mtr(,) As Double,ByRef mtrId(,) As Double, txt As TextBox) As Boolean
+		Try
+			Dim filas As Integer = txt.Lines.Length-1
+			Dim columnas As Integer = txt.Lines(0).Split(Convert.ToChar(",")).Length - 1
+			ReDim mtr(filas, columnas)
+			ReDim mtrId(filas, columnas)
+			
+			For	i = 0 To filas
+				For j = 0 To columnas
+					mtr(i,j) = Convert.ToDouble(txt.Lines(i).Split(Convert.ToChar(","))(j))
+					If i = j Then
+						mtrId(i,j) = 1
+					Else
+						mtrId(i,j) = 0
+					End If
+				Next
 			Next
-		Next
-		
-	End Sub
+			Return True
+		Catch e As FormatException
+			MsgBox("Ingresar solo numeros")
+		Catch e As IndexOutOfRangeException
+			MsgBox("Faltan valores por completar en la matriz.")
+		Catch e As Exception
+			MsgBox(e.ToString)
+			
+		End Try
+		Return False
+	End Function
 	
 	Sub mostrarResultado(matriz(,) As Double)
 		Try
@@ -60,44 +70,53 @@ Public Partial Class MainForm
 	End Sub	
 	
 	Sub calcInversa(matriz(,) As Double, matrizId(,) As Double)
-		
-		Dim filas As Integer = txtMatriz.Lines.Length - 1
-		Dim columnas As Integer = txtMatriz.Lines(0).Split(Convert.ToChar(",")).Length-1
-		
-		For y As Integer = 0 To columnas
-			Dim pivote As Double
+		Try
+			Dim filas As Integer = txtMatriz.Lines.Length - 1
+			Dim columnas As Integer = txtMatriz.Lines(0).Split(Convert.ToChar(",")).Length-1
 			
-			pivote = matriz(y,y)
-			If Not pivote = 1 Then
-				For x As Integer = 0 To filas
-					
-					matriz(y,x) /= pivote
-					matrizId(y,x) /= pivote
-					
-				Next x
-			End If
 			
-			For fil As Integer = 0 To filas
-				Dim valor As Double = matriz(fil, y)
-				If Not fil = y Then
-					For col As Integer = 0 To columnas
-						
-						matriz(fil, col) = (-valor*matriz(y,col))+ matriz(fil,col)
-						matrizId(fil, col)= (-valor*matrizId(y,col))+matrizId(fil, col)
-						
-					Next col
+			For y As Integer = 0 To columnas
+				Dim pivote As Double
+				
+				pivote = matriz(y,y)
+				If Not pivote = 1 Then
+					
+					For x As Integer = 0 To filas
+						matriz(y,x) /= pivote
+						matrizId(y,x) /= pivote
+					Next x
+					
 				End If
-			Next fil
-		Next y
-		
+				
+				For fil As Integer = 0 To filas
+					Dim valor As Double = matriz(fil, y)
+					If Not fil = y Then
+						For col As Integer = 0 To columnas
+							
+							matriz(fil, col) = (-valor*matriz(y,col))+ matriz(fil,col)
+							matrizId(fil, col)= (-valor*matrizId(y,col))+matrizId(fil, col)
+							
+						Next col
+					End If
+				Next fil
+			Next y
+		Catch e As Exception
+			MsgBox(e)
+		End Try 
 	End Sub
 	
 	Sub BtnCalcularClick(sender As Object, e As EventArgs)
 		txtMatrizInversa.Text = ""
-		cargar(matriz, matrizId, txtMatriz)
-		calcInversa(matriz, matrizId)
-		mostrarResultado(matrizId)
-		
+		If cargar(matriz, matrizId, txtMatriz) Then
+			If matriz(0,0) <> 0 Then
+				calcInversa(matriz, matrizId)
+				mostrarResultado(matrizId)
+			Else
+				MsgBox("La matriz no tiene solución")
+			End If
+		Else
+			MsgBox("No se han ingresado datos")
+		End If
 	End Sub
 	
 	Sub BtnLimpiarClick(sender As Object, e As EventArgs)
